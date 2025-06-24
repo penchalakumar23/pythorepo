1e58bfae92dc133d11e50ee7d02d20e31568ce09
Yaozhong Chen <yaozhong_chen@rush.edu>
2025-06-24 11:40:12 -0500

    removed  xmlsec

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,12 @@ echo "======Starting custom deployment script..."
 
 # --- Install system dependencies ---
 echo "======Running apt-get update and installing system dependencies..."
-apt-get update
-apt-get install -y build-essential python3-dev pkg-config libxml2-dev libxslt1-dev libxmlsec1-dev libxmlsec1-openssl
+# apt-get update
+# apt-get install -y build-essential python3-dev pkg-config libxml2-dev libxslt1-dev libxmlsec1-dev libxmlsec1-openssl
 
+sudo apt-get update
+sudo apt-get install -y build-essential python3-dev pkg-config libxml2-dev libxslt1-dev libxmlsec1-dev libxmlsec1-openssl
+ 
 # --- Run the default Oryx build for Python ---
 # This command will activate the virtual environment and run pip install -r requirements.txt
 # It's crucial to call the default Oryx build script to ensure your Python dependencies are installed.
